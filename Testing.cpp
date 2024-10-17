24ccf922f99e96a02a47884f67f310b9b06c945e
venezruk <126511409+venezruk@users.noreply.github.com>
2024-10-17 22:26:28 +0300

    Поправка комментария; добавление функции CompletionArrayOfArrays; изменение функции SetTesting

--- Testing.cpp
+++ Testing.cpp
@@ -8,7 +8,32 @@ using namespace std; /*позволяет использовать все иде
 		       из пространства имён std без указания
 		       префикса std::*/
 
-//Функция тестирования сортировки
+/// <summary>CompletionArrayOfArrays - Функция для заполнения массива указателями на массив</summary>
+/// <param name="ArrayOfArrays"> - Массив указателей на массив</param>
+/// <param name="firstarray"> - Первый указатель на массив</param>
+/// <param name="secondarray"> - Второй указатель на массив</param>
+/// <param name="thirdarray"> - Третий указатель на массив</param>
+/// <param name="fourtharray"> - Четвертый указатель на массив</param>
+void CompletionArrayOfArrays(int** ArrayOfArrays, int* firstarray, int* secondarray, int* thirdarray, int* fourtharray) {
+	ArrayOfArrays[0] = firstarray;
+	ArrayOfArrays[1] = secondarray;
+	ArrayOfArrays[2] = thirdarray;
+	ArrayOfArrays[3] = fourtharray;
+}
+
+/// <summary>SetTesting - Функция тестирования сортировки</summary>
+/// <param name="ArrayOfArrays"> - Массив указателей на массив</param>
+/// <param name="firstarray"> - Первый указатель на массив</param>
+/// <param name="secondarray"> - Второй указатель на массив</param>
+/// <param name="thirdarray"> - Третий указатель на массив</param>
+/// <param name="fourtharray"> - Четвертый указатель на массив</param>
+/// <param name="ArrayOfTrueArrays"> - Массив указателей на массив</param>
+/// <param name="truefirstarray"> - Первый указатель на массив с заведомо истинными значениями</param>
+/// <param name="truesecondarray"> - Второй указатель на массив с заведомо истинными значениями</param>
+/// <param name="truethirdarray"> - Третий указатель на массив с заведомо истинными значениями</param>
+/// <param name="truefourtharray"> - Четвертый указатель на массив с заведомо истинными значениями</param>
+/// <param name="size"> - Размеры массивов</param>
+/// <param name="IsTestFine"> - Принимет значение true, когда тесты завершены без ошибок</param>
 void SetTesting(void) {
 
 	bool IsTestFine = true;
@@ -19,56 +44,29 @@ void SetTesting(void) {
 	int* secondarray = new int[size] { 6, 5, 2, 5 };
 	int* thirdarray = new int[size] { -10, 7, 3, 6 };
 	int* fourtharray = new int[size] { 0, 1, 45, -9 };
-	int* fiftharray = new int[size] { -100, 50, 0, 100 };
 
-	ShakerSorting(firstarray, size);
-	ShakerSorting(secondarray, size);
-	ShakerSorting(thirdarray, size);
-	ShakerSorting(fourtharray, size);
-	ShakerSorting(fiftharray, size);
+	int** ArrayOfArrays = new int* [size];
+	CompletionArrayOfArrays(ArrayOfArrays, firstarray, secondarray, thirdarray, fourtharray);
+
+	for (int i = 0; i < size; i++) ShakerSorting(ArrayOfArrays[i], size);
 
 
 	int* truefirstarray = new int[size] { 1, 2, 3, 5 };
 	int* truesecondarray = new int[size] { 2, 5, 5, 6 };
 	int* truethirdarray = new int[size] { -10, 3, 6, 7 };
 	int* truefourtharray = new int[size] { -9, 0, 1, 45  };
-	int* truefiftharray = new int[size] { -100, 0, 50, 100 };
+
+	int** ArrayOfTrueArrays = new int* [size];
+	CompletionArrayOfArrays(ArrayOfTrueArrays, truefirstarray, truesecondarray, truethirdarray, truefourtharray);
 
 	for (int i = 0; i < size; i++) {
-
-		if (firstarray[i] != truefirstarray[i]) {
-			cout << "Ошибка при тестировании в массиве 1." << endl
-				 << "Ожидалось: " << truefirstarray[i] << endl
-				 << "Получено: " << firstarray[i] << endl;
-			IsTestFine = false;
-		}
-
-		if (secondarray[i] != truesecondarray[i]) {
-			cout << "Ошибка при тестировании в массиве 2." << endl
-				<< "Ожидалось: " << truesecondarray[i] << endl
-				<< "Получено: " << secondarray[i] << endl;
-			IsTestFine = false;
-		}
-
-		if (thirdarray[i] != truethirdarray[i]) {
-			cout << "Ошибка при тестировании в массиве 3." << endl
-				<< "Ожидалось: " << truethirdarray[i] << endl
-				<< "Получено: " << thirdarray[i] << endl;
-			IsTestFine = false;
-		}
-
-		if (fourtharray[i] != truefourtharray[i]) {
-			cout << "Ошибка при тестировании в массиве 4." << endl
-				<< "Ожидалось: " << truefourtharray[i] << endl
-				<< "Получено: " << fourtharray[i] << endl;
-			IsTestFine = false;
-		}
-
-		if (fiftharray[i] != truefiftharray[i]) {
-			cout << "Ошибка при тестировании в массиве 5." << endl
-				<< "Ожидалось: " << truefiftharray[i] << endl
-				<< "Получено: " << fiftharray[i] << endl;
-			IsTestFine = false;
+		for (int j = 0; j < size; j++) {
+			if (ArrayOfArrays[i][j] != ArrayOfTrueArrays[i][j]) {
+				cout << "Ошибка при тестировании в массиве " << i + 1 << "." << endl
+					<< "Ожидалось: " << ArrayOfTrueArrays[i][j] << endl
+					<< "Получено: " << ArrayOfArrays[i][j] << endl;
+				IsTestFine = false;
+			}
 		}
 	}
 
